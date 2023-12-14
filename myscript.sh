9ace54f90f77ead23f4a802d0fb6cec355e870a2
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-14 15:37:42 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -4,7 +4,7 @@ if [ ! -d "$venvname" ]; then
     python -m venv "$venvname"
 fi
 sleep 4
-. "$venvname/bin/activate"
+source "$venvname/bin/activate"
 pip install flask gunicorn
 
 gunicorn -w 4 -b 0.0.0.0:5230 --reload wsgi:app

aa30cfc45dab2650316d03bd7190fe1c955b96c0
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-14 15:54:13 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,10 +1,15 @@
 venvname="venv"
 
 if [ ! -d "$venvname" ]; then
+    echo "the env didn't existed"
     python -m venv "$venvname"
+    echo " now i have createdd"
 fi
+echo "waiting for 4"
 sleep 4
+echo " going to activate"
 source "$venvname/bin/activate"
+echo " it was activated"
 pip install flask gunicorn
 
 gunicorn -w 4 -b 0.0.0.0:5230 --reload wsgi:app

a9b4c134a9ca8a31fcb158df0eddf85ed6d2f6f0
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-14 15:24:57 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,26 +1,10 @@
-#!/bin/bash -x
 
-# Print current working directory
-echo "Current working directory: $(pwd)"
-
-# Define virtual environment name
-venvname="venv"
-
-# Check if the virtual environment exists
-if [ ! -d "$venvname" ]; then
-    echo "Creating virtual environment..."
-    ls
-    python -m venv "$venvname" || { echo "Failed to create virtual environment"; exit 1; }
+if [ ! -d "venv" ]; then
+    python -m venv "$venvname"
+    sleep 4
 fi
 
-# Activate the virtual environment
-source "$venvname/bin/activate" || { echo "Failed to activate virtual environment"; exit 1; }
+source "$venvname/bin/activate"
+pip install flask gunicorn
 
-# Print Python version
-echo "Using Python version: $(python --version)"
-
-# Install Flask and Gunicorn
-pip install flask gunicorn || { echo "Failed to install dependencies"; exit 1; }
-
-# Start the application with Gunicorn
 gunicorn -w 4 -b 0.0.0.0:5230 --reload wsgi:app

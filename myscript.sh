b21f554f4d60af02947fc76fb9b0ef40f30de8cc
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-14 12:54:06 +0500

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,10 @@
+venvname="venv"
+
+if [ ! -d "$venvname" ]; then
+    python -m venv "$venvname"
+fi
+
+source "$venvname/bin/activate"
+pip install flask gunicorn
+
+gunicorn -w 4 -b 0.0.0.0:5230 --reload wsgi:app

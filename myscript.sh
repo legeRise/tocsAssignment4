628ae63d375d30e1e3193ed454e4b19d947479d5
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-14 18:06:57 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,16 +1,10 @@
 venvname="venv"
 
 if [ ! -d "$venvname" ]; then
-    echo "the env didn't existed"
     python -m venv "$venvname"
-    echo " now i have created"
 fi
-echo "waiting for 4"
 sleep 4
-echo " going to activate"
 ls
 source "/var/lib/jenkins/workspace/finalpipeline/$venvname/bin/activate"
-echo " it was activated"
 pip install -r requirements.txt
-
 gunicorn -w 4 -b 0.0.0.0:5230 --reload wsgi:app
